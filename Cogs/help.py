b158b9478d87d2424372fedf01971b4242fa521f
Gooraeng <birdyoon1998@gmail.com>
2023-10-03 00:02:06 +0900

    231003

--- Cogs/help.py
+++ Cogs/help.py
@@ -14,9 +14,10 @@ class help(commands.Cog):
         embed=discord.Embed(title="Help", description="사용할 수 있는 명령어를 정리했습니다!", color=0x7fe6e4)
         embed.add_field(name="---------------", value="", inline=True)
         embed.add_field(name="1. Spec", value="차량의 성능을 확인합니다! 이 기능은 외부 데이터에 의해 작동되므로 언제든지 비활성화 될 수 있습니다.", inline=False)
-        embed.add_field(name="2. Ref", value="봇 제작에 있어 참고/활용된 자료의 출처를 알려줍니다!", inline=False)
-        embed.add_field(name="3. Link", value="봇 서버 링크를 알 수 있습니다!", inline=False)
-        embed.add_field(name="4. Ver", value="현재 봇의 마지막 업데이트 날짜를 알려줍니다!", inline=False)
+        embed.add_field(name="2. Clash", value="클럽 클래시 레퍼런스를 보여줍니다!", inline=False)
+        embed.add_field(name="3. Ref", value="봇 제작에 있어 참고/활용된 자료의 출처를 알려줍니다!", inline=False)
+        embed.add_field(name="4. Link", value="봇 서버 링크를 알 수 있습니다!", inline=False)
+        embed.add_field(name="5. Ver", value="현재 봇의 마지막 업데이트 날짜를 알려줍니다!", inline=False)
         
         await interaction.response.send_message("",embed=embed, ephemeral=True)
         
