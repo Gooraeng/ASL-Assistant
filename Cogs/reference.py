cd71aca05d4450f4ef68e1080f32da9ddb457ebf
Gooraeng <birdyoon1998@gmail.com>
2023-10-03 21:41:14 +0900

    231003

--- Cogs/reference.py
+++ Cogs/reference.py
@@ -11,10 +11,10 @@ class ref(commands.Cog):
     
     @app_commands.command(name='ref', description='봇의 레퍼런스 및 출처를 알려줍니다.')
     async def ref(self, interaction : discord.Interaction):
-        embed1=discord.Embed(title="레퍼런스 1 - MEI", url=settings.mei, description='클릭 시 링크로 이동합니다.',color=0x7fe6e4)
+        embed1=discord.Embed(title="레퍼런스 1 - MEI", url='https://www.mei-a9.info/cars', description='클릭 시 링크로 이동합니다.',color=0x7fe6e4)
         embed1.add_field(name="- 사용목적", value="아스팔트 9 차량 리스트 추출")
         
-        embed2=discord.Embed(title="레퍼런스 2 - A9 Database", description='클릭 시 링크로 이동합니다.', url=settings.a9_db, color=0x7fe6e4)
+        embed2=discord.Embed(title="레퍼런스 2 - A9 Database", description='클릭 시 링크로 이동합니다.', url='https://discord.gg/dVA7R9CXpB', color=0x7fe6e4)
         embed2.add_field(name="- 사용목적", value="아스팔트 9 차량 스펙 데이터 베이스 활용")
         
         embed3=discord.Embed(title="레퍼런스 3 - A9 Club Clash Database", description='Created by Sharp', color=0x7fe6e4)
