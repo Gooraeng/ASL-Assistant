476f09d7ef3f1921749a6a840264a055783b69f5
Gooraeng <birdyoon1998@gmail.com>
2023-10-03 18:04:12 +0900

    231003

--- Cogs/car_spec.py
+++ Cogs/car_spec.py
@@ -109,10 +109,10 @@ class spec(commands.Cog):
         
         # car_list.py 76째 줄 참고
         if car_name == 'KTM  X-BOW GTX':
-            await interaction.response.send_message('', embed=embed, file=discord.File(f'{settings.car_img}/KTM X-BOW GTX.png'),ephemeral=True)
+            await interaction.response.send_message('', embed=embed, file=discord.File(f'Car_spec_img/KTM X-BOW GTX.png'),ephemeral=True)
         else:
             try:
-                await interaction.response.send_message('', embed=embed, file=discord.File(f'{settings.car_img}/{car_name}.png'),ephemeral=True)
+                await interaction.response.send_message('', embed=embed, file=discord.File(f'Car_spec_img/{car_name}.png'),ephemeral=True)
             # 파일이 존재하지 않음
             except FileNotFoundError:
                 embed1 = discord.Embed(title='오류', description='찾고자 하는 차량의 정보가 없습니다. 나중에 다시 시도해주세요!')
