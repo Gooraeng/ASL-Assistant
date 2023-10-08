21c1e3b689f7c80db0e860c2bd458cb544e71db1
Gooraeng <birdyoon1998@gmail.com>
2023-10-09 03:49:37 +0900

    231009

--- Cogs/date.py
+++ Cogs/date.py
@@ -10,7 +10,7 @@ class date(commands.Cog):
     async def show_nengmyeon_bab(self, interaction : discord.Interaction):
         embed = discord.Embed(title='주의', description='일정이 정확하지 않을 수 있으니, 그저 참고만 해주세요!',colour=0xff0000)
         embed.add_field(name= '기간', value= '230927 ~ 231219', inline= False)
-        await interaction.response.send_message('', embed = embed, ephemeral = True, file = discord.File('./images/datasheet.png'))        
+        await interaction.response.send_message('', embed = embed, ephemeral = True, file = discord.File('./images/datesheet.png'))        
 
 async def setup(app):
     await app.add_cog(date(app))
