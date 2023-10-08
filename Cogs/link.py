c023bd09c87deb4b4fe8a4f006d745d26efd79a1
Gooraeng <birdyoon1998@gmail.com>
2023-10-09 04:37:51 +0900

    231009

--- Cogs/link.py
+++ Cogs/link.py
@@ -11,7 +11,7 @@ class link(commands.Cog):
         
     @app_commands.command(name="link",description="봇 서버 링크를 알 수 있습니다!")
     async def link(self, interaction : discord.Interaction):
-        await interaction.response.send_message("https://discord.gg/8dpAFYXk8s", ephemeral=False)
-        
+        await interaction.response.send_message("https://discord.gg/8dpAFYXk8s", ephemeral=False)     
+       
 async def setup(app):
     await app.add_cog(link(app))
