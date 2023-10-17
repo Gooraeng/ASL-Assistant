3092b027fb548eb1a2d2e8cb198b28a7f18631a1
Gooraeng <birdyoon1998@gmail.com>
2023-10-17 18:21:44 +0900

    231017

--- Cogs/clash.py
+++ Cogs/clash.py
@@ -2,9 +2,8 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import typing
-
 import numpy
-import csv
+from config import database
 
 
 class clash(commands.Cog):
@@ -15,12 +14,11 @@ class clash(commands.Cog):
     @app_commands.describe(area = '찾고자 하는 맵을 찾아보세요!', car_class = '클래스를 선택하세요', car_name ='어떤 차량을 찾아보시겠어요?')
     @app_commands.rename(area = '맵', car_class = '클래스', car_name = '차량')
     async def clashes(self, interaction: discord.Interaction, area : str, car_class : str, car_name : str):
+        
         # 맵과 차량이 다같이 대응되는 유튜브 링크 제공.
-        # 필요한 것 맵 리스트, 차량 리스트, 차량 리스트 안에 링크 append
         map_data = await database.ClubClash_Database_area()
         car_data = await database.ClubClash_Database_CarName()
         link_data = await database.ClubClash_Database_Link()
-        LT_data = await database.ClubClash_Database_LapTime()
         
         database1 = numpy.array(map_data)
         database2 = numpy.array(car_data)
@@ -35,27 +33,29 @@ class clash(commands.Cog):
         
         if link_data[same] == False:
             embed2 = discord.Embed(title="경고", description='데이터를 찾을 수 없습니다')
-            await interaction.response.send_message('',embed=embed2,ephemeral=True)
+            await interaction.response.send_message('',embed=embed2, ephemeral=True, delete_after=7)
     
     @clashes.autocomplete('area')
     async def area_autocompletion(
         self,
         interaciton : discord.Interaction,
-        current : str
+        current : str,
     ) -> typing.List[app_commands.Choice[str]]:
         
-        a = await database.ClubClash_Database_area()
-        map_list = list(set(a))
+        # 차량 리스트 선언
+        map_data = await database.ClubClash_Database_area()
+        
+        # cc_db 내 겹치는 차량 리스트가 존재하고, 리스트 검색 시 이를 허용하지 않게 하기 위한
+        # set을 이용하여 겹치는 차량이 없는 새 리스트 선언
+        filtered = list(set(map_data))
+        
         result1 = [
             app_commands.Choice(name=choice, value=choice)
-            for choice in map_list if current.lower() in choice.lower()
+            for choice in filtered if current.lower() in choice.lower()
         ]
     
-        if current.lower() is None:
-            result1 == None
-        else:
-            if len(result1) > 10:
-                result1 = result1[:10]
+        if len(result1) > 10:
+            result1 = result1[:10]
         return result1 
         
     
@@ -63,21 +63,35 @@ class clash(commands.Cog):
     async def class_autocompletion(
         self,
         interaction : discord.Interaction,
-        current : str
+        current : str,    
     ) -> typing.List[app_commands.Choice[str]]:
+        
+        # 리스트 선언
         map_data = await database.ClubClash_Database_area()
         class_data = await database.ClubClash_Database_Class()
+        
+        # area_autocompletion을 통해 찾으려는 맵과 관련된 요소를 불러옴.
+        # 여기선 딕셔너리를 이용하여 불러옴 >> dict_values(['Sacred Heart', ''])
+        # 리스트로 변환
         aa = list(interaction.namespace.__dict__.values())
+
+        
+        # 검색된 맵의 행들을 인덱스로 가지는 리스트를 선언함
+        # 이 때, map_data와 aa의 value가 일치하도록 필터링 (aa[0])
         rest_list = list(filter(lambda x: map_data[x] == str(aa[0]), range(len(map_data))))
         
-        emp_list = list()
         
+        
+        emp_list = list()
         for i in range(len(rest_list)):
             emp_list.append(class_data[rest_list[i]])
         
+        # emp_list 내 존재하는 중복 요소 제거
+        filetered = list(set(emp_list))
+        
         result2 = [
             app_commands.Choice(name=choice,value=choice)
-            for choice in list(set(emp_list)) if current.lower() in choice.lower()
+            for choice in filetered if current.lower() in choice.lower()
         ]
         
         return result2
@@ -89,29 +103,20 @@ class clash(commands.Cog):
         current : str, 
     ) -> typing.List[app_commands.Choice[str]]:
         
-        # club clash database row 1
+        # 리스트 선언
+        map_data = await database.ClubClash_Database_area()
         car_data = await database.ClubClash_Database_CarName()
-        
-        # club clash database row 2
         class_data = await database.ClubClash_Database_Class()
         
-        # club clash database 0
-        map_data = await database.ClubClash_Database_area()
-        
+        # class_autocompletion의 결과와 연동이 어려워 같은 방법 반복
         aa = list(interaction.namespace.__dict__.values())
-
-        # extract area numbers in map_ data list
         rest_list_1 = list(filter(lambda x: map_data[x] == str(aa[0]), range(len(map_data))))
         
-        # make a empty list that will contain car_data name list by using and matching -
-        # rest_list's area number.
-        # Car_data's length and map_data's one are same so you do not have to mind it.
-        emp_list_1 = list()
-        emp_list_2 = list()
-        for i in range(len(rest_list_1)):
-            emp_list_1.append(class_data[rest_list_1[i]])
+        
+        emp_list_1 = list(); emp_list_2 = list()
         
         for i in range(len(rest_list_1)):
+            emp_list_1.append(class_data[rest_list_1[i]])
             if class_data[rest_list_1[i]]== str(aa[1]):
                 emp_list_2.append(car_data[rest_list_1[i]])
                 
@@ -126,67 +131,6 @@ class clash(commands.Cog):
             
         return result3
         
-    
-class database(): 
-    async def ClubClash_Database():
-        data = list()
-        f = open('data/Club Clash Database.csv', "r",encoding='utf-8',newline='')
-        reader = csv.reader(f)
-        for row in reader:
-            data.append(row)
-        data.pop(0)
-        f.close()
-        return data
-
-    async def ClubClash_Database_area():
-        data = list()
-        f = open('data/Club Clash Database.csv', "r",encoding='utf-8',newline='')
-        reader = csv.reader(f)
-        for row in reader:
-            data.append(row[0])
-        data.pop(0)
-        f.close()
-        return data
-
-    async def ClubClash_Database_CarName():
-        data = list()
-        f = open('data/Club Clash Database.csv', "r",encoding='utf-8',newline='')
-        reader = csv.reader(f)
-        for row in reader:
-            data.append(row[1])
-        data.pop(0)
-        f.close()
-        return data
-
-    async def ClubClash_Database_Class():
-        data = list()
-        f = open('data/Club Clash Database.csv', "r",encoding='utf-8',newline='')
-        reader = csv.reader(f)
-        for row in reader:
-            data.append(row[2])
-        data.pop(0)
-        f.close()
-        return data
-    
-    async def ClubClash_Database_LapTime():
-        data = list()
-        f = open('data/Club Clash Database.csv', "r",encoding='utf-8',newline='')
-        reader = csv.reader(f)
-        for row in reader:
-            data.append(row[3])
-        data.pop(0)
-        f.close()
-        return data
-    
-    async def ClubClash_Database_Link():
-        data = list()
-        f = open('data/Club Clash Database.csv', "r",encoding='utf-8',newline='')
-        reader = csv.reader(f)
-        for row in reader:
-            data.append(row[4])
-        data.pop(0)
-        f.close()
-        return data
     
 async def setup(app):
     await app.add_cog(clash(app))
--- main.py
+++ main.py
@@ -1,104 +1,19 @@
 # 메인 코드 모음
 
-import asyncio
 import discord
 import os
-from discord.ext import commands, tasks
+from discord.ext import commands
+
 import settings
-import requests as req
-from bs4 import BeautifulSoup as beau
-import pandas as pd
-import csv
+import config
 
 
 intents = discord.Intents.default()
 intents.message_content = True
-app = commands.Bot(command_prefix="?",intents=intents)
+app = commands.Bot(command_prefix="?",intents=intents,)
 discord_api_token = str(settings.discord_api_token)
-car_img = settings.car_img
-car_list = str(settings.car_list)
+
     
-# 사이트로부터 리스트 정보 받아오기
-class manage():
-    async def make_new_car_list():
-        url = str(settings.list_url)
-    
-        response = req.get(url).text.encode('utf-8')
-        response = beau(response, 'lxml')
-    
-        target = response.find('table',{'id':'list', 'class':'table'})
-        thead = target.find_all('th')
-    
-        theadList = []
-
-    # 사이트 내 th, td 태그 제거 
-        theadLen = len(thead)
-        for i in range(0, theadLen):
-            thead = target.find_all('th')[i].text
-            theadList.append(thead)
-
-        tdTags = target.find_all('td')
-
-        rowList=[]
-        columnList = []
-
-        tdTagsLen = len(tdTags)
-        for i in range(0, tdTagsLen):
-            element = tdTags[i].text
-            columnList.append(element)
-            if i % 2 ==1:
-                rowList.append(columnList)
-                columnList=[]
-        result = pd.DataFrame(rowList, columns=theadList)
-    # 태그 제거 결과 확인
-        print(result)
-    
-    # csv 파일로 우선 저장 [차량, 클래스] 꼴로 저장됨
-        f = open(car_list,'w',encoding='utf-8',newline='')
-        writer = csv.writer(f)
-        writer.writerow(theadList)
-        writer.writerows(rowList)
-        f.close()
-        
-# make_new_car_list에서 나온 [차량, 클래스 중] [차량]만 활용할 수 있게 csv 파일 편집 
-    async def utilize_list():
-        data = list()
-        f = open('data/A9 Car List.csv', "r",encoding='utf-8',newline='')
-        reader = csv.reader(f)
-        for row in reader:
-            data.append(row[0])
-        data.pop(0)
-        f.close()
-        return data
-
-# 차량 사진 리스트 추출 및 csv 파일 간 대조
-    async def check_update():
-        await manage.make_new_car_list()  
-        data = await manage.utilize_list()  
-        car_img_list = list()
-        for filename in os.listdir('Car_spec_img'):
-            if filename.endswith(".png") or filename.endswith(".jpg"):
-                car_img_list.append(filename[:-4])
-            elif filename.endswith(".jpeg"):
-                car_img_list.append(filename[:-5])
-    
-    # 업데이트 된 차량(들)의 리스트 선언 
-        check_new = list(set(data)- set(car_img_list))
-
-    # 리스트 대조 후 일치 시
-    # KTM X-BOW GTX는 이미 존재하는 차량인데 data 리스트에서는 띄어쓰기가 두 번 적용된 것이 확인되어 억지로 맞게 만듬
-        if len(list(data))-len(list(car_img_list))==0:
-            if 'KTM  X-BOW GTX' in data:
-                print('추가된 차량이 없습니다!')
-
-    # 리스트 대조 후 불일치 시
-    # 76번 줄과 같은 사유
-        else:
-            if 'KTM  X-BOW GTX' in data:
-                check_new.remove('KTM  X-BOW GTX')
-                print('차량 업데이트 발견: '+ str(check_new))
-
-
 # 확장 기능(명령어) 로드
 async def load_extensions():
     for filename in os.listdir("Cogs"):
@@ -110,32 +25,44 @@ async def load_extensions():
                 print(f"파일 오류 발생 : {filename}")
             except commands.ExtensionNotFound:
                 print(f"{filename[:-3]} 파일이 존재하지 않습니다.")
+            except commands.ExtensionAlreadyLoaded:
+                print(f"{filename[:-3]} 이 이미 로드되었습니다.")
        
-
 # 봇 이벤트
 @app.event
 async def on_ready():
-    await load_extensions()
-    await manage.make_new_car_list()
-    print('---------------------------------------')
-    await manage.check_update()
-    print('---------------------------------------')   
     print(f"{app.user.name} 준비 중")
     try:
+        await load_extensions()
+        print('---------------------------------------')
+        await config.check_update()
+        print('---------------------------------------')  
+        
         synced = await app.tree.sync()
         print(f"명령어 {len(synced)}개 사용 가능")
-        current_status = discord.Game(name='A9차량 정보제공')
+        current_status = discord.Game(name='ASL에 정보제공')
         await app.change_presence(status=discord.Status.online,activity=current_status)
         print(f"{app.user.name}이(가) 준비되었습니다!")
                     
     except Exception as e:
-        print(e)        
+        print(e)
+        
+async def on_message(self, message : discord.Message) -> None:
+    if message.author.bot:
+        return
+
+async def on_guild_join(self, guild : discord.Guild) -> None:
+    if guild.id in self.blacklist:
+        await guild.leave()
+    
+
+            
 # 에러 관리
 async def on_command_error(ctx, interaction : discord.Interaction, error):
     # 존재하지 않는 명령어 에러처리
     if isinstance(error, commands.CommandNotFound):
         embed = discord.Embed(title="오류",description="존재하지 않는 명령어입니다.",colour=0xFF0000)
-        await interaction.response.send_message("",embed=embed,ephemeral=True) 
+        await interaction.response.send_message("",embed=embed,ephemeral=True, delete_after= 5) 
     
     # 명령어 오류 처리
     else:
@@ -143,12 +70,10 @@ async def on_command_error(ctx, interaction : discord.Interaction, error):
         embed.add_field(name="상세", value=f"```{error}```")
         await interaction.response.send_message("",embed=embed,ephemeral=True)  
 
-@tasks.loop(hours=24)
-async def update():
-    print('---------------------------------------')
-    await manage.check_update()
-    print('업데이트 완료')   
-       
+
+def main():
+    app.run(discord_api_token)
+
 # 메인 실행
 if __name__ == '__main__':
-    app.run(discord_api_token)
+    main()
