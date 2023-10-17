b2df57cc558dc1f8701ab946172fe682c5bfab94
Gooraeng <birdyoon1998@gmail.com>
2023-10-17 21:18:39 +0900

    231017

---
+++ settings.py
@@ -0,0 +1,16 @@
+# 디코 세팅
+# 민감한 파일
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+discord_api_token = os.getenv('discord_api_token')
+list_url = os.getenv('list_url')
+car_list = os.getenv('car_list')
+club_clash_database = os.getenv('club_clash_database')
+car_img = os.getenv('car_img')
+mei = os.getenv('mei')
+a9_db = os.getenv('a9_db')
+cc_db = os.getenv('cc_db')
