e2f9424c9e41f000ec09d24f63529a7c28993ab1
Gooraeng <birdyoon1998@gmail.com>
2023-10-28 03:36:06 +0900

    231028

--- Cogs/clash.py
+++ Cogs/clash.py
@@ -34,22 +34,23 @@ class clash(commands.Cog):
         
         same = int(numpy.intersect1d(a, b))
         
-        try:
-            if link_data[same] == False:
-                embed2 = discord.Embed(title="경고", description='데이터를 찾을 수 없습니다')
-                await interaction.response.send_message('',embed=embed2, ephemeral=True, delete_after=7)
-            else:
+        if link_data[same] is None:
+            embed2 = discord.Embed(title="경고", description='데이터를 찾을 수 없습니다')
+            await interaction.response.send_message('',embed=embed2, ephemeral=True, delete_after=7)
+        
+        else:
+            try:
                 await interaction.response.send_message(f'{link_data[same]}')
         
-        except Exception:
-            await interaction.response.defer(ephemeral= True)
-            await asyncio.sleep(4)
-            if link_data[same] == False:
-                embed2 = discord.Embed(title="경고", description='데이터를 찾을 수 없습니다')
-                await interaction.followup.send('',embed=embed2, ephemeral=True, delete_after=7)
-            else:
-                await interaction.followup.send(f'{link_data[same]}')
-            
+            except Exception:
+                await interaction.response.defer(ephemeral= True)
+                await asyncio.sleep(4)
+                if link_data[same] == False:
+                    embed2 = discord.Embed(title="경고", description='데이터를 찾을 수 없습니다')
+                    await interaction.followup.send('',embed=embed2, ephemeral=True, delete_after=7)
+                else:
+                    await interaction.followup.send(f'{link_data[same]}')
+                
             
     
     @clashes.autocomplete('area')
