ea9982b2cd60383fc8c7e31283d3e1c3954527e4
Gooraeng <birdyoon1998@gmail.com>
2023-10-30 13:46:29 +0900

    TEST

--- Cogs/utils/print_time.py
+++ Cogs/utils/print_time.py
@@ -1,5 +1,5 @@
 import urllib.request
-
+from discord.ext import tasks
 #-*- coding: euc-kr -*-
 import urllib.request
 month = {'Jan':'01', 'Feb':'02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06', \
@@ -7,9 +7,9 @@ month = {'Jan':'01', 'Feb':'02', 'Mar':'03', 'Apr':'04', 'May':'05', 'Jun':'06',
 
 url = 'https://time.navyism.com/'
 
-date = urllib.request.urlopen(url).headers['Date'][5:-4]
-
-d, m, y, hour, min, sec = date[:2], month[date[3:6]], date[7:11], date[12:14], date[15:17], date[18:]
-
+@tasks.loop(seconds=0.5)
 async def get_UTC():
+    date = urllib.request.urlopen(url).headers['Date'][5:-4]
+    d, m, y, hour, min, sec = date[:2], month[date[3:6]], date[7:11], date[12:14], date[15:17], date[18:]
+
     return f'{y}년 {m}월 {d}일 {hour}시 {min}분 {sec}초'
