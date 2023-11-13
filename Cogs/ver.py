dc1a00290896e8dab5df8bbdd3b8d356a62f463a
Gooraeng <birdyoon1998@gmail.com>
2023-11-14 00:40:53 +0900

    231113 - ver modified

--- Cogs/ver.py
+++ Cogs/ver.py
@@ -1,5 +1,5 @@
 # 현재 봇의 마지막 업데이트 버전을 알려주는 명령어
-# Last Update : 231111
+# Last Update : 231113
 
 import discord
 
@@ -16,7 +16,7 @@ class ver(commands.Cog):
     @app_commands.command(name="ver",description="현재 봇의 마지막 업데이트 날짜를 알려줍니다!")
     @app_commands.guild_only()
     async def ver(self, interaction : discord.Interaction):
-        embed=discord.Embed(title="마지막 업데이트", description="2023/11/11", color=0x7fe6e4)
+        embed=discord.Embed(title="마지막 업데이트", description="2023/11/13", color=0x7fe6e4)
         embed.add_field(name='',value='정보가 없는 차량이 있을 수 있습니다. 하지만, 꼭 업데이트 될 겁니다!', inline=False)
         embed.add_field(name='',value='**<경고>** 이 메세지는 10초 뒤 사라집니다!')
         await interaction.response.send_message("",embed=embed, ephemeral=True,delete_after=10)
