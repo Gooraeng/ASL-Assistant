772d98828b864305b763f9170132b881461babf7
Gooraeng <birdyoon1998@gmail.com>
2023-11-20 03:15:02 +0900

    231120 - link dm 내에서 사용 가능

--- Cogs/link.py
+++ Cogs/link.py
@@ -17,11 +17,29 @@ class link(commands.Cog):
     @app_commands.command(name="link",description="봇 서버 링크를 알 수 있습니다!")
     async def link(self, interaction : discord.Interaction):
         await interaction.response.send_message("https://discord.gg/8dpAFYXk8s", ephemeral=False)     
-        ch = self.app.get_channel(log_channel)
+        ch = self.app.get_channel(log_channel)        
         
-        DmChannel = discord.DMChannel.id  
+        no_variable_embed = discord.Embed(title= '정상 실행', description= f'link', colour= etc)
+        no_variable_embed.add_field(name='시간(UTC)', value= f'{await print_time.get_UTC()} (UTC)', inline= False)
+        no_variable_embed.add_field(name='서버명', value= f'{interaction.guild.name}', inline= True)
+        no_variable_embed.add_field(name='채널명', value= f'{interaction.channel.name}', inline= True)
+        no_variable_embed.add_field(name='유저', value= f'{interaction.user.display_name}', inline= True)
+        no_variable_embed.add_field(name='서버 ID', value= f'{interaction.guild.id}', inline= True)
+        no_variable_embed.add_field(name='채널 ID', value= f'{interaction.channel.id}', inline= True)
+        no_variable_embed.add_field(name='유저 ID', value= f'{interaction.user.id}', inline= True)
         
-        if interaction.channel.id == DmChannel:
+        await ch.send(embed = no_variable_embed)
+        confirm = f"정상 실행 > {await print_time.get_UTC()} > link > 서버: {interaction.guild.name} > 채널 : {interaction.channel.name} > 실행자: {interaction.user.display_name}" ; print(confirm)
+
+        
+       
+    
+    @link.error
+    async def link_error_is_dm(self, interaction : discord.Interaction, error : app_commands.AppCommandError):
+        ch = self.app.get_channel(log_channel) 
+        
+        if isinstance(error, app_commands.CommandInvokeError):
+            
             no_variable_embed_dm = discord.Embed(title= '정상 실행', description= f'link', colour= etc)
             no_variable_embed_dm.add_field(name='시간(UTC)', value= f'{await print_time.get_UTC()} (UTC)', inline= False)
             no_variable_embed_dm.add_field(name='채널명 (ID)', value= f'DM ({interaction.channel.id})', inline= True)
@@ -30,20 +48,7 @@ class link(commands.Cog):
             
             await ch.send(embed = no_variable_embed_dm)
             confirm = f"정상 실행 > {await print_time.get_UTC()} > link > 서버: - > 채널 : DM ({interaction.channel.id}) > 실행자: {interaction.user.display_name}" ; print(confirm)
-        
-        else:
-            no_variable_embed = discord.Embed(title= '정상 실행', description= f'link', colour= etc)
-            no_variable_embed.add_field(name='시간(UTC)', value= f'{await print_time.get_UTC()} (UTC)', inline= False)
-            no_variable_embed.add_field(name='서버명', value= f'{interaction.guild.name}', inline= True)
-            no_variable_embed.add_field(name='채널명', value= f'{interaction.channel.name}', inline= True)
-            no_variable_embed.add_field(name='유저', value= f'{interaction.user.display_name}', inline= True)
-            no_variable_embed.add_field(name='서버 ID', value= f'{interaction.guild.id}', inline= True)
-            no_variable_embed.add_field(name='채널 ID', value= f'{interaction.channel.id}', inline= True)
-            no_variable_embed.add_field(name='유저 ID', value= f'{interaction.user.id}', inline= True)
-            
-            await ch.send(embed = no_variable_embed)
-            confirm = f"정상 실행 > {await print_time.get_UTC()} > link > 서버: {interaction.guild.name} > 채널 : {interaction.channel.name} > 실행자: {interaction.user.display_name}" ; print(confirm)
-        
-        
+    
+                
 async def setup(app):
     await app.add_cog(link(app))
