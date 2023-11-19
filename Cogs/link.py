7060b3a459ca23e7a57048117d0a24b3e63e7826
Gooraeng <birdyoon1998@gmail.com>
2023-11-20 02:52:55 +0900

    231120 - /ver 삭제

--- Cogs/link.py
+++ Cogs/link.py
@@ -18,18 +18,21 @@ class link(commands.Cog):
     async def link(self, interaction : discord.Interaction):
         await interaction.response.send_message("https://discord.gg/8dpAFYXk8s", ephemeral=False)     
         ch = self.app.get_channel(log_channel)
-        
-        no_variable_embed = discord.Embed(title= '정상 실행', description= f'link', colour= etc)
-        no_variable_embed.add_field(name='시간(UTC)', value= f'{await print_time.get_UTC()} (UTC)', inline= False)
-
-        
+               
         
         if interaction.channel == discord.DMChannel:
-            no_variable_embed.add_field(name='채널명 (ID)', value= f'DM ({interaction.channel.id})', inline= True)
-            no_variable_embed.add_field(name='유저', value= f'{interaction.user.display_name}', inline= True)
-            no_variable_embed.add_field(name='유저 ID', value= f'{interaction.user.id}', inline= True)
+            no_variable_embed_dm = discord.Embed(title= '정상 실행', description= f'link', colour= etc)
+            no_variable_embed_dm.add_field(name='시간(UTC)', value= f'{await print_time.get_UTC()} (UTC)', inline= False)
+            no_variable_embed_dm.add_field(name='채널명 (ID)', value= f'DM ({interaction.channel.id})', inline= True)
+            no_variable_embed_dm.add_field(name='유저', value= f'{interaction.user.display_name}', inline= True)
+            no_variable_embed_dm.add_field(name='유저 ID', value= f'{interaction.user.id}', inline= True)
             
+            await ch.send(embed = no_variable_embed_dm)
+            confirm = f"정상 실행 > {await print_time.get_UTC()} > link > 서버: - > 채널 : DM ({interaction.channel.id}) > 실행자: {interaction.user.display_name}" ; print(confirm)
+        
         else:
+            no_variable_embed = discord.Embed(title= '정상 실행', description= f'link', colour= etc)
+            no_variable_embed.add_field(name='시간(UTC)', value= f'{await print_time.get_UTC()} (UTC)', inline= False)
             no_variable_embed.add_field(name='서버명', value= f'{interaction.guild.name}', inline= True)
             no_variable_embed.add_field(name='채널명', value= f'{interaction.channel.name}', inline= True)
             no_variable_embed.add_field(name='유저', value= f'{interaction.user.display_name}', inline= True)
