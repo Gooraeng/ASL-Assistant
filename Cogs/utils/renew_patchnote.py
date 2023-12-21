22d6daf29641c1313cf545dc3511403a775f10cb
Gooraeng <birdyoon1998@gmail.com>
2023-12-21 21:35:54 +0900

    231221 - (hotfix) 패치노트 최신화 오류 수정

--- Cogs/utils/renew_patchnote.py
+++ Cogs/utils/renew_patchnote.py
@@ -8,44 +8,30 @@ from discord.ext import tasks
 from discord import Embed
 from .print_time import get_UTC
 
-target = 'https://asphaltlegends.com/news/'
-
-res = requests.get(target)
-html = res.text
-soup = BS(html, 'html.parser')
-
-
-p_tags = 'ck ck-content a6aJuu'
-
-@tasks.loop(minutes= 5)
-async def get_patchnote_link():
-    if res.status_code == 200:
-        link = soup.find_all('link')[12]['href'][:-3]
-        
-        return link
-    
-    else:
-        return None
 
 
 @tasks.loop(minutes= 5)
 async def get_patchnote_embed():
+    target = 'https://asphaltlegends.com/news/'
+
+    res = requests.get(target)
+    html = res.text
+    soup = BS(html, 'html.parser')
+    
     if res.status_code == 200:
         title = soup.find_all(class_ = 'text-h3 pb-4 uppercase')[0].text
         date = soup.find_all(class_ = 'text-lg font-semibold text-dark-blue-500 md:text-xl')[0].text
         summary = soup.find_all(class_ = 'py-4 text-justify text-lg font-semibold md:text-xl')[0].text
+        link = soup.find_all('link')[12]['href'][:-3]
+        elements = soup.find('div', class_ = 'ck ck-content a6aJuu')
         
-        embed = Embed(title= f'**[{date}] {title}**', description= f'제목을 클릭 시 패치노트를 확인하실 수 있습니다!\n', url= await get_patchnote_link(), colour= 0xFF0055)
+        embed = Embed(title= f'**[{date}] {title}**', description= f'제목을 클릭 시 패치노트를 확인하실 수 있습니다!\n', url= link, colour= 0xFF0055)
         embed.set_author(name= 'ASPHALT 9 : LEGENDS', icon_url= 'https://asphalt9.assets.gameloft.com/assets/a9_logo_12de60294a.png', url= 'https://asphaltlegends.com/')
         embed.set_footer(text= f'{await get_UTC()} (UTC)')
         embed.add_field(name= '< 요약 >', value= f'{summary}\n-----------------------------------', inline= False)
-        elements = soup.find('div', class_ = p_tags)
         
             
         if elements:
-            if len(elements) > 1000:
-                embed.add_field(name= '경고', value= '패치노트가 너무 길어 다 보여줄 수 없습니다. 직접 확인해주세요!')
-                return embed
             
             for tag in elements.children:
                     
@@ -56,8 +42,6 @@ async def get_patchnote_embed():
                 if tag.name == 'h3':
                     embed.add_field(name= f'# {tag.get_text(strip = True)}', value='', inline= False)
 
-
-    
         
         return embed
     
